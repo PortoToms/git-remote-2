bd6002b8b1c29b2bff1a985760d04905e670837a
Munawir Fadly <pbmunawirfadly@gmail.com>
2024-11-28 00:09:14 +0700

    merubah isi file index.php

--- index.php
+++ index.php
@@ -8,5 +8,11 @@
 <body>
     <h1>Masih belajar git remote</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe autem quibusdam velit reiciendis ullam? Exercitationem beatae asperiores esse dolore modi architecto, explicabo omnis tempore a qui quis officiis. Et, aspernatur!</p>
+
+    <ul>
+        <li>list1</li>
+        <li>list2</li>
+        <li>list3</li>
+    </ul>
 </body>
 </html>
